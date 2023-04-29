b879b06bc5836d3a9ef84a0d5551f56f22afab19
max-loktionov <studreno@gmail.com>
2023-04-29 11:24:34 +0300

    ADD: settings for library lightbox

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,26 +1,38 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { cardSrc } from "./imageImport";
 import { slidesSet } from "./imageImport";
 import { GallaryList, CardImg } from "./Gallery.styled";
-import { ReactComponent as MyCloseIcon } from "../../assets/facebook.svg";
+import MyPrevIcon from "../../assets/images/gallery/prev.png";
+import MyNextIcon from "../../assets/images/gallery/next.png";
+import MyCloseIcon from "../../assets/images/gallery/close.png";
 
 import Lightbox from "yet-another-react-lightbox";
-import "yet-another-react-lightbox/styles.css";
+// import "yet-another-react-lightbox/styles.css";
 
 export default function Gallery() {
   const [open, setOpen] = useState(false);
-
+  const ref = useRef(null);
   return (
     <>
       <Lightbox
+        slides={slidesSet}
         open={open}
         close={() => setOpen(false)}
         styles={{
           container: { backgroundColor: "rgba(0, 0, 0, .7)", padding: "32px" },
-          icon: { color: "red", width: "40px", height: "40px" },
-          button: { width: "100px", height: "100px" },
+          // icon: { color: "red", width: "40px", height: "40px" },
+          button: { color: "red", width: "100px", height: "100px", position: "absolute", bottom: "-4500%" },
         }}
-        slides={slidesSet}
+        render={{
+          iconPrev: () => <img src={MyPrevIcon} />,
+          iconNext: () => <img src={MyNextIcon} />,
+          iconClose: () => <img src={MyCloseIcon} />,
+          buttonPrev: slidesSet.length <= 1 ? () => null : undefined,
+          buttonNext: slidesSet.length <= 1 ? () => null : undefined,
+        }}
+        controller={{ ref }}
+        on={{ click: () => ref.current?.close() }}
+        carousel={{ finite: true }}
       />
       <GallaryList>
         {cardSrc.slice(0, 6).map((card, idx) => {
