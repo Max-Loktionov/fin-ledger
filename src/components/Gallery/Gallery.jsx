d52723196ae22e7c0f08981600330bb4d6b1c04b
max-loktionov <studreno@gmail.com>
2023-04-29 12:53:53 +0300

    FIX: change lightbox gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -11,36 +11,46 @@ import Lightbox from "yet-another-react-lightbox";
 
 export default function Gallery() {
   const [open, setOpen] = useState(false);
+  const [currentItem, setCurrentItem] = useState(0);
+
+  const handleOpenClick = (id) => {
+    setCurrentItem(id);
+    setOpen(true);
+    console.log("current", id);
+  };
+
   const ref = useRef(null);
   return (
     <>
       <Lightbox
         slides={slidesSet}
+        index={currentItem}
+        labels={{ Next: "Next slide" }}
         open={open}
         close={() => setOpen(false)}
         styles={{
           container: { backgroundColor: "rgba(0, 0, 0, .7)", padding: "32px" },
-          // icon: { color: "red", width: "40px", height: "40px" },
-          button: { color: "red", width: "100px", height: "100px", position: "absolute", bottom: "-4500%" },
+          button: { width: "40%", height: "40%", opacity: "0.3" },
+          icon: { width: "72px", height: "72px" },
         }}
         render={{
-          iconPrev: () => <img src={MyPrevIcon} />,
-          iconNext: () => <img src={MyNextIcon} />,
-          iconClose: () => <img src={MyCloseIcon} />,
-          buttonPrev: slidesSet.length <= 1 ? () => null : undefined,
-          buttonNext: slidesSet.length <= 1 ? () => null : undefined,
+          // iconPrev: () => <img src={MyPrevIcon} />,
+          // iconNext: () => <img src={MyNextIcon} />,
+          // iconClose: () => <img src={MyCloseIcon} />,
+          buttonPrev: currentItem === 0 ? () => null : undefined,
+          buttonNext: currentItem === slidesSet.length - 1 ? () => null : undefined,
         }}
         controller={{ ref }}
-        on={{ click: () => ref.current?.close() }}
-        carousel={{ finite: true }}
+        on={{ click: () => ref.previous.close() }}
+        carousel={{ finite: true, closeOnBackdropClick: false, touchAction: "pan-y" }}
       />
       <GallaryList>
         {cardSrc.slice(0, 6).map((card, idx) => {
           return (
-            <picture key={idx} onClick={() => setOpen(true)}>
+            <picture key={idx}>
               <source srcSet={`${card._1x_webp} 1x, ${card._2x_webp} 2x`} type="image/webp" loading="lazy" />
               <source srcSet={`${card._1x} 1x, ${card._2x} 2x`} type="image/jpeg" loading="lazy" />
-              <CardImg src={card._1x} alt={card.alt} />
+              <CardImg src={card._1x} alt={card.alt} onClick={() => handleOpenClick(idx)} />
             </picture>
           );
         })}
